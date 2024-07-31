e2b1a69f7eb41cde6f4b70c19a6a84b1befd372f
Ayoob Florival <aflorival@tomsrivermua.org>
2024-07-31 07:34:45 -0400

    boilerplate for automove util

---
+++ trmua-utils/utils/AutoMove.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace trmua_utils.utils
+{
+    public class AutoMove
+    {
+        private CancellationToken? _cts;
+        public event Action<string>? LogMessage;
+
+        public async Task AutoMoveAsync(string TargetFolder, string DestinationFolder, Dispatcher dispatcher)
+        {
+            _cts= new CancellationToken();
+
+        }
+    }
+}
